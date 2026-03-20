e64bfb9f4df566011db2b96370030705bfcc5ca5
Sumiran Dahal <sumirandahal46@gmail.com>
2026-03-20 10:52:11 +0545

    fix: invoke handler registered

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+use crate::state::AppState;
+
+pub mod commands;
 pub mod domain;
 pub mod error;
 pub mod repository;
@@ -10,9 +13,16 @@ pub async fn run() {
     utils::env::load_env_files();
     utils::env::init_tracing();
 
+    let state = AppState::from_env().await.unwrap();
+
     tracing::info!("Starting application...");
 
     tauri::Builder::default()
+        .manage(state)
+        .invoke_handler(tauri::generate_handler![
+            commands::auth::signup,
+            commands::auth::signin
+        ])
         .run(tauri::generate_context!())
         .unwrap_or_else(|error| tracing::error!(?error, "error while running tauri application"));
 }
